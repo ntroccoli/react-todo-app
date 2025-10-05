6c727285e164173972a70ef6feb34a48f1434987
Nelson Troccoli Jr <ntroccoli@hotmail.com>
2025-10-05 07:37:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple React To-Do List App
+
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl">Simple React To-Do List App</h1>
+    </div>
+  );
+}
+
+export default App;
